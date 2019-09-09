6284b931a10da8cfb48babd4e11d3d7380d1754e
Mitch Downey <mitchldowney@gmail.com>
2019-09-09 00:31:19 -0500

    Update playerEvents.ts

--- src/services/playerEvents.ts
+++ src/services/playerEvents.ts
@@ -85,17 +85,12 @@ module.exports = async () => {
     const { nextTrack, track } = x
 
     if (Platform.OS === 'ios') {
-      const currentId = await PVTrackPlayer.getCurrentTrack()
-
-      const id = currentId && currentId === track ? track : nextTrack
-      await PVTrackPlayer.seekTo(0)
-
-      // Wait for the PVTrackPlayer to seek to 0 before syncing
       setTimeout(async () => {
+        const currentId = await PVTrackPlayer.getCurrentTrack()
+        const id = currentId && currentId === track ? track : nextTrack
         if (!track) return
         await syncNowPlayingItemWithTrack(id)
-      }, 1000)
-
+      }, 2000)
     } else if (Platform.OS === 'android') {
       await syncNowPlayingItemWithTrack(nextTrack)
     }
