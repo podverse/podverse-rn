67cc6ffbd0983b6988b1f036782ed1c2951032e6
Mitch Downey <mitchldowney@gmail.com>
2019-12-05 00:05:51 -0600

    Update purchase.ios.ts

--- src/services/purchase.ios.ts
+++ src/services/purchase.ios.ts
@@ -1,16 +1,8 @@
-
-import * as RNIap from 'react-native-iap'
 import { updateAppStorePurchaseStatus } from './appStorePurchase'
 
-export const iosHandlePurchaseStatusCheck = async (productId: string, transactionId: string, transactionReceipt: string) => {
+export const iosHandlePurchaseStatusCheck = async (transactionReceipt: string) => {
   try {
-    const response = await updateAppStorePurchaseStatus(transactionReceipt)
-    const { finishedTransactionIds } = response.data
-    if (finishedTransactionIds && Array.isArray(finishedTransactionIds)) {
-      for (const id of finishedTransactionIds) {
-        await RNIap.finishTransactionIOS(id)
-      }
-    }
+    await updateAppStorePurchaseStatus(transactionReceipt)
   } catch (error) {
     throw (error)
   }
