09528f8849973b5cc1ac242b046b9fe60b8721b1
Mitch Downey <mitchldowney@gmail.com>
2020-02-23 23:28:38 -0600

    Reduce font size on Android

--- src/resources/Fonts.ts
+++ src/resources/Fonts.ts
@@ -1,6 +1,18 @@
+import { Platform } from 'react-native'
 import { IFonts } from './Interfaces'
 
-export const Fonts: IFonts = {
+const sizes = Platform.OS === 'android' ?
+({
+  sizes: {
+    tiny: 12,
+    xs: 13,
+    sm: 14,
+    md: 16,
+    lg: 17,
+    xl: 18
+  }
+}) :
+({
   sizes: {
     tiny: 13,
     xs: 14,
@@ -8,7 +20,11 @@ export const Fonts: IFonts = {
     md: 17,
     lg: 18,
     xl: 19
-  },
+  }
+})
+
+export const Fonts: IFonts = {
+  sizes,
   weights: {
     thin: '300',
     normal: '400',
