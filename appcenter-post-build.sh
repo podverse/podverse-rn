caa04281ae589c9f947a68965be7436f7d4f4c93
Creon Creonopoulos <kreonjr@hotmail.com>
2020-05-02 20:57:32 -0500

    Updated post build script to email on bs upload success

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -11,9 +11,13 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
 
     if [ "$APPCENTER_BRANCH" == "develop" ];
      then
-       APP_URL=$(curl -u "$BS_USERNAME:$BS_KEY" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk")
+        APP_URL=$(curl -u "$BS_USERNAME:$BS_KEY" -X POST "https://api-cloud.browserstack.com/app-automate/upload" -F "file=@$APPCENTER_OUTPUT_DIRECTORY/app-release.apk")
 
-       echo "BROWSERSTACK APP URL IS $APP_URL"
+        TO_ADDRESS="creon@podverse.fm"
+        SUBJECT="Browserstack build upload complete"
+        BODY="Build uploaded to browserstack. \n\n APP_URL: ${APP_URL}!\n\n"
+
+        echo -e ${BODY} | mail -s "${SUBJECT} - Success!" ${TO_ADDRESS}
     else
         echo "Current branch is $APPCENTER_BRANCH"
     fi
