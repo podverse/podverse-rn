fbb8edd4d970a50def0b3cb9621153bec09a1b48
Mitch Downey <mitchldowney@gmail.com>
2020-05-09 20:17:31 -0500

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$PLATFORM" == "ios" ] ; then
-    cd ./ios && rm -rf Pods && pod cache clean --all && pod install && cd ..
-else
-    npx jetify
-fi
-
-# Creates an .env from ENV variables for use with react-native-config
-ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
-printf "Creating an .env file with the following whitelist:\n"
-printf "%s\n" $ENV_WHITELIST
-set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
-printf "\n.env created with contents:\n\n"
-cat .env
-
 echo "temporary test build code"
         ls
         echo $STAGE_SSH_PRIVATE_KEY > touch ./podverse_stage_rsa
@@ -31,3 +17,19 @@ echo "temporary test build code"
 
         echo "END: SSH into stage server and restore stage database before running tests"
 echo "end temporary test code"
+
+if [ "$PLATFORM" == "ios" ] ; then
+    cd ./ios && rm -rf Pods && pod cache clean --all && pod install && cd ..
+else
+    npx jetify
+fi
+
+# Creates an .env from ENV variables for use with react-native-config
+ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
+printf "Creating an .env file with the following whitelist:\n"
+printf "%s\n" $ENV_WHITELIST
+set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
+printf "\n.env created with contents:\n\n"
+cat .env
+
+
