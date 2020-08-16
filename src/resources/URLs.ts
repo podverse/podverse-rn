ef7a2661fc1831a05091597ab9adae31584e26ec
Mitch Downey <mitchldowney@gmail.com>
2020-08-15 19:28:36 -0500

    Update URLs.ts

--- src/resources/URLs.ts
+++ src/resources/URLs.ts
@@ -1,13 +1,13 @@
 import Config from 'react-native-config'
 
 const protocol = 'https://'
-const domain = Config.WEB_DOMAIN || 'stage.podverse.fm'
+const domain = Config.RN_WEB_DOMAIN || 'stage.podverse.fm'
 const root = protocol + domain
 
 export const URLs = {
   about: `${root}/about`,
   // tslint:disable-next-line: max-line-length
-  baseUrl: Config.API_DOMAIN || 'https://api.stage.podverse.fm/api/v1',
+  baseUrl: Config.RN_API_DOMAIN || 'https://api.stage.podverse.fm/api/v1',
   clip: `${root}/clip/`,
   contact: 'https://docs.google.com/forms/d/e/1FAIpQLSe-1_1qmv5Z21ZLc37KWke3cXFluItnzmstjqGwm9_BT7BGRg/viewform',
   episode: `${root}/episode/`,
