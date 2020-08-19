1c9f34fc56d3310e25f797db38abab7bb164b189
Mitch Downey <mitchldowney@gmail.com>
2020-08-19 00:54:01 -0500

    Update SearchScreen.tsx

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -154,7 +154,8 @@ export class SearchScreen extends React.Component<Props, State> {
     this.props.navigation.navigate(PV.RouteNames.SearchPodcastScreen, {
       podcast,
       viewType,
-      isSearchScreen: true
+      isSearchScreen: true,
+      screenStackPrefix: PV.RouteNames.ScreenStackPrefix.Search
     })
   }
 
