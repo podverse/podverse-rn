4d4a76a17ce27ad21626acca9258550270bd8a88
Creon Creonopoulos <kreonjr@hotmail.com>
2020-09-04 19:37:25 -0500

    Added custom i18n tranlator. Removed dependency

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /** @format */
-import { setI18nConfig } from './src/lib/i18n'
-setI18nConfig()
-
 const { AppRegistry } = require('react-native')
-const App = require("./App").default
-import { name as appName } from "./app.json"
+const App = require('./App').default
+import { name as appName } from './app.json'
 import './src/lib/extensions'
 
 AppRegistry.registerComponent(appName, () => App)
