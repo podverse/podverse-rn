12a2b7aa68d68e2f835f8fd60f8d21947fac6fe7
Mitch Downey <mitchldowney@gmail.com>
2020-09-28 00:38:42 -0500

    Update DeepLinks.ts

--- src/resources/DeepLinks.ts
+++ src/resources/DeepLinks.ts
@@ -4,39 +4,33 @@ export const DeepLinks = {
   prefix: Config.DEEP_LINK_PREFIX,
   AddByRSSPodcastFeedUrl: {
     path: 'podcast-by-feed-url/add*',
-    pathPrefix: 'podcast-by-feed-url',
-    pathWithId: (id: string) => `podcast-by-feed-url/${id}`
+    pathPrefix: 'podcast-by-feed-url'
   },
   Clip: {
     path: 'clip/:mediaRefId',
-    pathPrefix: 'clip',
-    pathWithId: (id: string) => `clip/${id}`
+    pathPrefix: 'clip'
   },
   Episode: {
     path: 'episode/:episodeId',
-    pathPrefix: 'episode',
-    pathWithId: (id: string) => `episode/${id}`
+    pathPrefix: 'episode'
   },
   Playlist: {
     path: 'playlist/:playlistId',
-    pathPrefix: 'playlist',
-    pathWithId: (id: string) => `playlist/${id}`
+    pathPrefix: 'playlist'
   },
   Playlists: {
     path: 'playlists'
   },
   Podcast: {
     path: 'podcast/:podcastId',
-    pathPrefix: 'podcast',
-    pathWithId: (id: string) => `podcast/${id}`
+    pathPrefix: 'podcast'
   },
   Podcasts: {
     path: 'podcasts'
   },
   Profile: {
     path: 'profile/:userId',
-    pathPrefix: 'profile',
-    pathWithId: (id: string) => `profile/${id}`
+    pathPrefix: 'profile'
   },
   Profiles: {
     path: 'profiles'
