8f2a823ec054e58ed0a6990cc888c35d8d01e728
Mitch Downey <mitchldowney@gmail.com>
2021-01-31 00:15:33 -0600

    Update MediaPlayerCarouselViewer.tsx

--- src/components/MediaPlayerCarouselViewer.tsx
+++ src/components/MediaPlayerCarouselViewer.tsx
@@ -38,22 +38,19 @@ export class MediaPlayerCarouselViewer extends React.PureComponent<Props, State>
     const { reduceBottomWrapperHeight } = this.state
     const { currentChapter, nowPlayingItem = {} } = player
     const { isLoading } = screenPlayer
-    const { clipIsOfficialChapter } = nowPlayingItem
     let { clipId, clipEndTime, clipStartTime, clipTitle, podcastImageUrl } = nowPlayingItem
     const imageStyle = [styles.image, { height: imageHeight, width: imageWidth }]
     let clipUrl = ''
 
     // If a clip is currently playing, then load the clip info.
-    // Else if a chapter is currently playing, then load the chapter info.
-    if (currentChapter) {
-      if (!clipId || clipIsOfficialChapter) {
-        clipId = currentChapter.id
-        clipEndTime = currentChapter.endTime
-        clipUrl = currentChapter.linkUrl
-        clipStartTime = currentChapter.startTime
-        clipTitle = currentChapter.title
-        podcastImageUrl = currentChapter.imageUrl || podcastImageUrl
-      }
+    // Else if a chapter is currently playing, then override with the chapter info.
+    if (currentChapter && !clipId) {
+      clipId = currentChapter.id
+      clipEndTime = currentChapter.endTime
+      clipUrl = currentChapter.linkUrl
+      clipStartTime = currentChapter.startTime
+      clipTitle = currentChapter.title
+      podcastImageUrl = currentChapter.imageUrl || podcastImageUrl
     }
 
     const imageWrapperStylePadding = clipId ? { padding: 16 } : { paddingHorizontal: 16, paddingTop: 16 }
