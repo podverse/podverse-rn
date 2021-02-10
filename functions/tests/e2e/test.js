e1324910df044bb40ba55152b59935bde4ace6a0
KyleFDowney <capntechbeard@gmail.com>
2021-02-10 10:19:18 -0600

    cont. rn tests

--- functions/tests/e2e/test.js
+++ functions/tests/e2e/test.js
@@ -146,14 +146,14 @@ const performScrollUp = async () => {
   logPerformance('Scrollup performed')
 }
 
-const confirmAndroidAlert = async (testLabel) => {
+const confirmAndroidAlert = async () => {
   logTestInfo(true, null, 'Confirm Android Alert')
   const el = await driver.element('id', 'android:id/button1')
   await el.click()
   logTestInfo(false, null, 'Confirm Android Alert')
 }
 
-const cancelAndroidAlert = async (testLabel) => {
+const cancelAndroidAlert = async () => {
   logTestInfo(true, null, 'Cancel Android Alert')
   const el = await driver.element('id', 'android:id/button2')
   await el.click()
@@ -168,6 +168,7 @@ testProps(
 */
 
 const goBack = true
+const noTestLabel = null
 
 const runTests = async (customCapabilities) => {
   Object.assign(capabilities, customCapabilities)
@@ -189,30 +190,109 @@ const runTests = async (customCapabilities) => {
 
     await driver.waitForElementByAccessibilityId('alert_yes_allow_data')
     await elementByIdAndClickAndTest('alert_yes_allow_data', 'podcasts_screen_view')
+
+    // Guest User Tests
+
+    // await elementByIdAndClickAndTest('tab_library_screen', 'my_library_screen_view')
 //  ***Need ID for My Library tab***
+//    tab_my_library_screen
 //  Select Podcast
-    // await elementByIdAndClickAndTest('podcasts_screen_podcast_item_0', 'podcast_screen_view', null, goBack)
+    // await elementByIdAndClickAndTest('podcasts_screen_podcast_item_0', 'podcast_screen_view', noTestLabel, goBack)
+//  Select Episode
+    // await driver.back()
+    // await driver.back()
+    // await elementByIdAndClickAndTest('tab_episodes_screen', 'episodes_screen_view')
+    // await elementByIdAndClickAndTest('episodes_screen_episode_item_0_top_view_nav', 'episode_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('episodes_screen_episode_item_0_bottom_view_nav', 'episode_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('tab_clips_screen', 'clips_screen_view')
+    // // My Library > Downloads < Queue < History
+    // await elementByIdAndClickAndTest('tab_more_screen', 'more_screen_view')
+    // await elementByIdAndClickAndTest('more_screen_add_podcast_by_rss_cell', 'add_podcast_by_rss_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('more_screen_settings_cell', 'settings_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_view')
+    // await elementByIdClick('auth_screen_reset_password_button')
+    // await driver.back()
+    // await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_view')
+    // await elementByIdClick('auth_screen_sign_up_button')
+    // await driver.back()
+    // await elementByIdAndClickAndTest('more_screen_membership_cell', 'membership_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('more_screen_about_cell', 'about_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('more_screen_terms_of_service_cell', 'terms_of_service_screen_view', noTestLabel, goBack)
+    // await elementByIdAndClickAndTest('nav_search_icon', 'search_screen_view')
+    // await sendKeysToElementById('search_screen_search_bar', 'Very Bad Wizards', 'Search for Very Bad Wizards')
+    // await driver.sleep(5000)
+    // await driver.waitForElementByAccessibilityId('search_screen_podcast_item_0')
+
+    // Add Custom RSS Feed
+
+
+    // Registered User Tests
+
+
+    // await driver.waitForElementByAccessibilityId('alert_yes_allow_data')
+    // await elementByIdAndClickAndTest('alert_yes_allow_data', 'podcasts_screen_view')
+
+    //Membership Screen
+
+    await elementByIdAndClickAndTest('tab_more_screen', 'more_screen_view')
+    await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_sign_up_button')
+    await elementByIdAndClickAndTest('auth_screen_sign_up_button', 'membership_screen_view', null, goBack)
+  
+    // Reset Password
+
+    // await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_sign_up_button')
+    // await elementByIdAndClickAndTest('auth_screen_reset_password_button', 'reset_password_submit')
+    
+      // ***Failing to enter text?
+    // await sendKeysToElementById('reset_password_email_text_input', 'TestEmail@ThisIsATest.com') 
+    
+
+    // await elementByIdClick('reset_password_submit')
+    // await confirmAndroidAlert()
+
+    // Login - Valid
+    
+    await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_sign_up_button')
+    await sendKeysToElementById('login_email_text_input', 'premium@stage.podverse.fm', 'Valid Login Email Input')
+    await sendKeysToElementById('login_password_text_input', 'Aa!1asdf', 'Valid Login Password Input')
+    await elementByIdClick('login_submit_button')
+    await driver.sleep(4000)
+    await cancelAndroidAlert()
+
+    // await elementByIdAndClickAndTest('tab_library_screen', 'my_library_screen_view')
+//  ***Need ID for My Library tab***
+//    tab_my_library_screen
+//  Select Podcast
+    // await elementByIdAndClickAndTest('podcasts_screen_podcast_item_0', 'podcast_screen_view', noTestLabel, goBack)
 //  Select Episode
     // await driver.back()
     // await driver.back()
     await elementByIdAndClickAndTest('tab_episodes_screen', 'episodes_screen_view')
-    // await elementByIdAndClickAndTest('episodes_screen_episode_item_0', 'episode_screen_view', null, goBack) // ***Failing?***
+    await elementByIdAndClickAndTest('episodes_screen_episode_item_0_top_view_nav', 'episode_screen_view', noTestLabel, goBack)
+    await elementByIdAndClickAndTest('episodes_screen_episode_item_0_bottom_view_nav', 'episode_screen_view', noTestLabel, goBack)
     await elementByIdAndClickAndTest('tab_clips_screen', 'clips_screen_view')
     // My Library > Downloads < Queue < History
     await elementByIdAndClickAndTest('tab_more_screen', 'more_screen_view')
-    await elementByIdAndClickAndTest('more_screen_add_podcast_by_rss_cell', 'add_podcast_by_rss_screen_view', null, goBack)
-    await elementByIdAndClickAndTest('more_screen_settings_cell', 'settings_screen_view', null, goBack)
-    await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_view')
-    await elementByIdClick('auth_screen_reset_password_button')
+    await elementByIdAndClickAndTest('more_screen_add_podcast_by_rss_cell', 'add_podcast_by_rss_screen_view', noTestLabel, goBack)
+    await elementByIdAndClickAndTest('more_screen_about_cell', 'about_screen_view', noTestLabel, goBack)
+    await elementByIdAndClickAndTest('more_screen_terms_of_service_cell', 'terms_of_service_screen_view', noTestLabel, goBack)
+
+    //Settings
+    await elementByIdAndClickAndTest('more_screen_settings_cell', 'settings_screen_view')
+    // await elementbyIdToggle('settings_screen_dark_mode_switch')
+    await elementbyIdToggle('settings_screen_only_allow_downloading_when_connected_to_wifi_switch')
+    await elementByIdClick('settings_screen_limit_the_number_of_downloaded_episodes_switch')
+    await elementByIdClick('settings_screen_dialog_update_download_limit_yes_button')
+    await elementbyIdToggle('settings_screen_censor_nsfw_text_switch')
+    await elementbyIdToggle('settings_screen_offline_mode_switch')
     await driver.back()
-    await elementByIdAndClickAndTest('more_screen_login_cell', 'auth_screen_view')
-    await elementByIdClick('auth_screen_sign_up_button')
-    await driver.back()
-    await elementByIdAndClickAndTest('more_screen_membership_cell', 'membership_screen_view', null, goBack)
-    await elementByIdAndClickAndTest('more_screen_about_cell', 'about_screen_view', null, goBack)
-    await elementByIdAndClickAndTest('more_screen_terms_of_service_cell', 'terms_of_service_screen_view', null, goBack)
+
+
+    //Search
     await elementByIdAndClickAndTest('nav_search_icon', 'search_screen_view')
-    // Add Custom RSS Feed
+    await sendKeysToElementById('search_screen_search_bar', 'Very Bad Wizards', 'Search for Very Bad Wizards')
+    await driver.sleep(5000)
+    await driver.waitForElementByAccessibilityId('search_screen_podcast_item_0')
 
   } catch (error) {
     console.log('runTests error: ', error)
