fab25304dcae479df27ec018dff3c56f3e066df8
Mitch Downey <mitchldowney@gmail.com>
2021-06-20 23:28:03 -0500

    Update ValueTagSetupScreen.tsx

--- src/screens/ValueTagSetupScreen.tsx
+++ src/screens/ValueTagSetupScreen.tsx
@@ -69,63 +69,64 @@ export class ValueTagSetupScreen extends React.Component<Props, State> {
 
     return (
       <View style={styles.content} {...testProps(`${testIDPrefix}_view`)}>
-            <View style={styles.itemWrapper}>
-              <SwitchWithText
-                onValueChange={this._showLNPaySetup}
-                subText={translate('Enable Lightning Pay switch description')}
-                testID={`${testIDPrefix}_lnpay_mode`}
-                text={translate(`Enable LN Pay`)}
-                value={lnpayEnabled}
-              />
-              {lnpayEnabled && (
-                <TextInput
-                  alwaysShowEyebrow
-                  eyebrowTitle={translate('Boost Amount')}
-                  keyboardType='numeric'
-                  onBlur={() => {
-                    const { localBoostAmount } = this.state
-                    if (Number(localBoostAmount) && Number(localBoostAmount) > MINIMUM_BOOST_PAYMENT) {
-                      updateGlobalBoostAmount(Number(localBoostAmount))
-                    } else {
-                      updateGlobalBoostAmount(MINIMUM_BOOST_PAYMENT)
-                      this.setState({ localBoostAmount: MINIMUM_BOOST_PAYMENT.toString() })
-                    }
-                  }}
-                  onSubmitEditing={() => Keyboard.dismiss()}
-                  onChangeText={(newText: string) => {
-                    this.setState({ localBoostAmount: newText })
-                  }}
-                  testID={`${testIDPrefix}_boost_amount_text_input`}
-                  value={`${localBoostAmount}`}
-                  wrapperStyle={styles.textInputWrapper}
-                />
-              )}
-              {lnpayEnabled && (
-                <TextInput
-                  alwaysShowEyebrow
-                  eyebrowTitle={translate('Streaming Amount')}
-                  keyboardType='numeric'
-                  onBlur={() => {
-                    const { localStreamingAmount } = this.state
-                    if (
-                      Number(localStreamingAmount)
-                      && Number(localStreamingAmount) > MINIMUM_STREAMING_PAYMENT) {
-                      updateGlobalStreamingAmount(Number(localStreamingAmount))
-                    } else {
-                      updateGlobalStreamingAmount(MINIMUM_STREAMING_PAYMENT)
-                      this.setState({ localStreamingAmount: MINIMUM_STREAMING_PAYMENT.toString() })
-                    }
-                  }}
-                  onChangeText={(newText: string) => {
-                    this.setState({ localStreamingAmount: newText })
-                  }}
-                  onSubmitEditing={() => Keyboard.dismiss()}
-                  testID={`${testIDPrefix}_streaming_amount_text_input`}
-                  value={`${localStreamingAmount}`}
-                  wrapperStyle={styles.textInputWrapper}
-                />
-              )}
-            </View>
+        <View style={styles.itemWrapper}>
+          <SwitchWithText
+            onValueChange={this._showLNPaySetup}
+            subText={lnpayEnabled ? '' : translate('Enable Lightning Pay switch description')}
+            testID={`${testIDPrefix}_lnpay_mode`}
+            text={translate(`Enable LN Pay`)}
+            value={lnpayEnabled}
+            wrapperStyle={styles.switchWithTextWrapper}
+          />
+          {lnpayEnabled && (
+            <TextInput
+              alwaysShowEyebrow
+              eyebrowTitle={translate('Boost Amount')}
+              keyboardType='numeric'
+              onBlur={() => {
+                const { localBoostAmount } = this.state
+                if (Number(localBoostAmount) && Number(localBoostAmount) > MINIMUM_BOOST_PAYMENT) {
+                  updateGlobalBoostAmount(Number(localBoostAmount))
+                } else {
+                  updateGlobalBoostAmount(MINIMUM_BOOST_PAYMENT)
+                  this.setState({ localBoostAmount: MINIMUM_BOOST_PAYMENT.toString() })
+                }
+              }}
+              onSubmitEditing={() => Keyboard.dismiss()}
+              onChangeText={(newText: string) => {
+                this.setState({ localBoostAmount: newText })
+              }}
+              testID={`${testIDPrefix}_boost_amount_text_input`}
+              value={`${localBoostAmount}`}
+              wrapperStyle={styles.textInputWrapper}
+            />
+          )}
+          {lnpayEnabled && (
+            <TextInput
+              alwaysShowEyebrow
+              eyebrowTitle={translate('Streaming Amount')}
+              keyboardType='numeric'
+              onBlur={() => {
+                const { localStreamingAmount } = this.state
+                if (
+                  Number(localStreamingAmount)
+                  && Number(localStreamingAmount) > MINIMUM_STREAMING_PAYMENT) {
+                  updateGlobalStreamingAmount(Number(localStreamingAmount))
+                } else {
+                  updateGlobalStreamingAmount(MINIMUM_STREAMING_PAYMENT)
+                  this.setState({ localStreamingAmount: MINIMUM_STREAMING_PAYMENT.toString() })
+                }
+              }}
+              onChangeText={(newText: string) => {
+                this.setState({ localStreamingAmount: newText })
+              }}
+              onSubmitEditing={() => Keyboard.dismiss()}
+              testID={`${testIDPrefix}_streaming_amount_text_input`}
+              value={`${localStreamingAmount}`}
+              wrapperStyle={styles.textInputWrapper}
+            />
+          )}
+        </View>
       </View>
     )
   }
@@ -139,12 +140,17 @@ const styles = StyleSheet.create({
     paddingHorizontal:15
   },
   itemWrapper: {
-    marginBottom: 24
+    marginBottom: 24,
+    width: '100%'
+  },
+  switchWithTextWrapper: {
+    marginBottom: 24,
+    marginTop: 8
   },
   text: {
     fontSize: PV.Fonts.sizes.xxl
   },
   textInputWrapper: {
-    marginVertical: 20
+    marginVertical: 8
   }
 })
