010df86d1b2de0ed05e82e715b16eda6cc32a509
KyleFDowney <capntechbeard@gmail.com>
2021-07-22 16:12:03 -0500

    cont. rn tests

--- functions/tests/e2e/tests/01-launchApp.js
+++ functions/tests/e2e/tests/01-launchApp.js
@@ -1,8 +1,11 @@
 const { getDriver } = require('../driver/driverFactory')
-const { elementByIdAndClickAndTest } = require('../driver/helpers/elements')
+const { elementByIdAndClickAndTest, elementByIdClick } = require('../driver/helpers/elements')
 
 const test01_launchApp = async () => {
   const driver = getDriver()
+
+  await driver.waitForElementByAccessibilityId('tracking_consent_screen_yes_enable_tracking_button')
+  await elementByIdClick('tracking_consent_screen_yes_enable_tracking_button')
   await driver.waitForElementByAccessibilityId('alert_yes_allow_data')
   await elementByIdAndClickAndTest('alert_yes_allow_data', 'podcasts_screen_view')
 }
