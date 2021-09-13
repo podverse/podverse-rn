5609199e468145783b4cbcf3c1755a67114eb6a1
Mitch Downey <mitchldowney@gmail.com>
2021-09-13 00:01:54 -0500

    Update accessibility for ClipTableCell

--- src/screens/ClipsScreen.tsx
+++ src/screens/ClipsScreen.tsx
@@ -460,6 +460,7 @@ export class ClipsScreen extends React.Component<Props, State> {
                 handleDismiss: this._handleCancelPress,
                 handleDownload: this._handleDownloadPressed,
                 handleDeleteClip: this._handleDeleteClip,
+                includeGoToPodcast: true,
                 includeGoToEpisodeInEpisodesStack: true
               },
               'clip'
