32cbb5f761e16df26e206d1e7769e2a72c385d8d
Creon Creonopoulos <kreonjr@hotmail.com>
2021-09-18 22:00:35 -0500

    Added offline banner when offline mode is enabled

--- src/components/PVTabBar.tsx
+++ src/components/PVTabBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BottomTabBar } from 'react-navigation-tabs'
 import { useGlobal } from 'reactn'
-import { View } from '../components'
+import {StyleSheet} from "react-native"
+import { View, Text } from '../components'
 import { PV } from '../resources'
 import { darkTheme } from '../styles'
+import { translate } from '../lib/i18n'
 import { MiniPlayer } from './MiniPlayer'
 
 type Props = {
@@ -13,10 +15,15 @@ type Props = {
 export const PVTabBar = (props: Props) => {
   const { navigation } = props
   const [player] = useGlobal<any>('player')
+  const [offlineModeEnabled] = useGlobal<any>('offlineModeEnabled')
 
   return (
-    <View>
+    <View testID="tabbar">
       {player && player.showMiniPlayer && player.nowPlayingItem && <MiniPlayer navigation={navigation} />}
+      {offlineModeEnabled && 
+      <View testID="offline-banner" style={styles.offlineBanner}>
+        <Text testID="offline-banner-text" style={styles.offlineBannerText}>{translate("OFFLINE MODE ENABLED")}</Text>
+      </View>}
       <BottomTabBar
         {...props}
         activeTintColor={PV.Colors.skyLight}
@@ -26,3 +33,15 @@ export const PVTabBar = (props: Props) => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  offlineBanner: {
+    backgroundColor: PV.Colors.yellow, 
+    width:"100%", 
+    alignItems:"center"
+  },
+  offlineBannerText: {
+    fontWeight:PV.Fonts.weights.bold,
+    color:PV.Colors.black
+  }
+})
