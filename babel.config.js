dab0a0ea7f0e6a8b32b5f8c053e383a091118ef2
Mitch Downey <mitchldowney@gmail.com>
2021-09-27 19:25:43 -0500

    Bump react-native-track-player to 2.1.1

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset']
-};
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+         extensions: ['.tsx', '.ts', '.js', '.json']
+      }
+    ],
+    'react-native-reanimated/plugin'
+  ]
+}
