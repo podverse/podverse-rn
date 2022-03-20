6b9151bc4237453a052e1ac46b40ccc2437bfd17
Creon Creonopoulos <kreonjr@hotmail.com>
2022-03-19 20:10:28 -0500

    Updated searchbar ui for android

--- src/screens/PodcastScreen.tsx
+++ src/screens/PodcastScreen.tsx
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-community/async-storage'
 import debounce from 'lodash/debounce'
 import { convertToNowPlayingItem, Episode } from 'podverse-shared'
-import { StyleSheet, View as RNView } from 'react-native'
+import { Platform, StyleSheet, View as RNView } from 'react-native'
 import Dialog from 'react-native-dialog'
 import { NavigationStackOptions } from 'react-navigation-stack'
 import React from 'reactn'
@@ -253,12 +253,14 @@ export class PodcastScreen extends React.Component<Props, State> {
                 this._updateCredentialsState()
                 // Adding a no time setTimeout for the listref to have populated
                 // in the next event loop otherwise, there will be no ref to call scroll to yet
-                setTimeout(() => {
-                  this.listRef?.scrollToOffset({
-                    animated: false,
-                    offset: PV.FlatList.ListHeaderHiddenSearchBar.contentOffset.y
+                if (Platform.OS === 'ios') {
+                  setTimeout(() => {
+                    this.listRef?.scrollToOffset({
+                      animated: false,
+                      offset: PV.FlatList.ListHeaderHiddenSearchBar.contentOffset.y
+                    })
                   })
-                })
+                }
               }
             )
           } catch (error) {
@@ -969,6 +971,7 @@ export class PodcastScreen extends React.Component<Props, State> {
                 renderItem={this._renderItem}
                 listRef={(ref) => (this.listRef = ref)}
                 showNoInternetConnectionMessage={offlineModeEnabled || showNoInternetConnectionMessage}
+                contentOffset={{ x: 0, y: 30 }}
               />
             )}
             <ActionSheet
