c4ca1938c0767cf637975ac7e3698a0226e3f873
Mitch Downey <mitchldowney@gmail.com>
2022-05-28 13:46:35 -0500

    Update MembershipScreen.tsx

--- src/screens/MembershipScreen.tsx
+++ src/screens/MembershipScreen.tsx
@@ -283,7 +283,7 @@ const comparisonData = [
     column2: true,
     accessibilityLabel: translate('Podcasting 2.0 transcripts'),
     videoUrl:
-      'https://peertube.podverse.fm/static/streaming-playlists/hls/e02afb1e-5426-4377-88bf-86ebbb9edd9c/24090cbd-ff14-4c6f-9d40-a2847cf2f9f8-1080-fragmented.mp4'
+      'https://peertube.podverse.fm/static/streaming-playlists/hls/67764a9d-12e0-4c25-9ae8-746efe484fa2/91b7fc2c-5c9f-48ff-a786-7fb5515931ba-1080-fragmented.mp4'
   },
   {
     text: translate('OPML import and export'),
