d370a98831686805f9d75f425582e826286a5b98
Mitch Downey <mitchldowney@gmail.com>
2022-09-08 22:08:13 -0500

    Add liveItem.chatIRCURL to MediaPlayerCarouselChatRoom

---
+++ src/components/MediaPlayerCarouselChatRoom.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native'
+import React from 'reactn'
+import { WebView } from 'react-native-webview'
+import { translate } from '../lib/i18n'
+import { TableSectionSelectors, View } from '.'
+
+type Props = {
+  navigation?: any
+  width: number
+}
+
+const testIDPrefix = 'media_player_carousel_chat'
+
+export class MediaPlayerCarouselChatRoom extends React.PureComponent<Props> {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    const { width } = this.props
+    const { player } = this.global
+    const liveItem = player?.episode?.liveItem
+    const chatIRCURL = liveItem?.chatIRCURL || ''
+
+    return (
+      <View style={[styles.wrapper, { width }]} testID={`${testIDPrefix}_view`} transparent>
+        <TableSectionSelectors disableFilter includePadding selectedFilterLabel={translate('Chat Room')} />
+        <WebView
+          accessible={false}
+          containerStyle={styles.wrapper}
+          originWhitelist={['*']}
+          source={{ uri: chatIRCURL }}
+          style={{ backgroundColor: 'transparent' }}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  }
+})
