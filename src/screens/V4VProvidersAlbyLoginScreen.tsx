c93bf6c1bb2fe29ce6866c1aef313e05bbe37e10
Mitch Downey <mitchldowney@gmail.com>
2022-12-10 02:44:45 -0600

    Fix deeplink with Alby login on iOS

--- src/screens/V4VProvidersAlbyLoginScreen.tsx
+++ src/screens/V4VProvidersAlbyLoginScreen.tsx
@@ -2,8 +2,10 @@ import { StyleSheet } from 'react-native'
 import { WebView } from 'react-native-webview'
 import React from 'reactn'
 import { NavDismissIcon, View } from '../components'
+import { PV } from '../resources'
 import { trackPageView } from '../services/tracking'
-import { v4vAlbyGenerateOAuthUrl } from '../services/v4v/providers/alby'
+import { v4vAlbyCheckConnectDeepLink, v4vAlbyGenerateOAuthUrl } from '../services/v4v/providers/alby'
+import { v4vAlbyHandleConnect } from '../state/actions/v4v/providers/alby'
 
 type Props = {
   navigation: any
@@ -37,11 +39,43 @@ export class V4VProvidersAlbyLoginScreen extends React.Component<Props, State> {
   }
 
   render() {
+    const { navigation } = this.props
     const { url } = this.state
 
     return (
       <View style={styles.wrapper}>
-        <WebView overScrollMode='never' removeClippedSubviews source={{ uri: url }} style={{ opacity: 0.99 }} />
+        <WebView
+          dataDetectorTypes={['link']}
+          originWhitelist={['https://', PV.DeepLinks.prefix]}
+          onShouldStartLoadWithRequest={(request) => {
+            // This is mostly duplicated in PodcastsScreen deep links.
+            const route = request.url.replace(/.*?:\/\//g, '')
+            const splitPath = route.split('/')
+            const domain = splitPath[0] ? splitPath[0] : ''
+            const urlParamsString = splitPath[splitPath.length - 1].split('?')[1]
+            const urlParams: any = {}
+            if (urlParamsString) {
+              const urlParamsArr = urlParamsString.split('&')
+              if (urlParamsArr.length) {
+                urlParamsArr.forEach((param) => {
+                  const [key, value] = param.split('=')
+                  urlParams[key] = value
+                })
+              }
+            }
+
+            // NOTE: HandleConnect is in two places due to iOS webview issue.
+            // https://github.com/react-native-webview/react-native-webview/issues/2681
+            if (v4vAlbyCheckConnectDeepLink(domain) && urlParams?.code) {
+              v4vAlbyHandleConnect(navigation, urlParams.code)
+            } else {
+              return true
+            }
+
+            return false
+          }}
+          source={{ uri: url }}
+        />
       </View>
     )
   }
