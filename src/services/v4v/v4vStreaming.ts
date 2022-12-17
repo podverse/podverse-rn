9623f9c92d24b68dea7afd443467989e0c021174
Mitch Downey <mitchldowney@gmail.com>
2022-12-17 15:09:27 -0600

    Normalize streaming sats interval with playback speed

--- src/services/v4v/v4vStreaming.ts
+++ src/services/v4v/v4vStreaming.ts
@@ -1,13 +1,57 @@
+import { Platform } from 'react-native'
 import { getGlobal, setGlobal } from 'reactn'
 import { translate } from '../../lib/i18n'
 import { processValueTransactionQueue, saveStreamingValueTransactionsToTransactionQueue } from '../../services/v4v/v4v'
 import { getBoostagramItemValueTags, v4vGetActiveProviderInfo } from '../../state/actions/v4v/v4v'
 import {
+  getPlaybackSpeed,
   playerCheckIfStateIsPlaying,
   playerGetState
 } from '../player'
 
-let valueStreamingIntervalSecondCount = 1
+let valueStreamingAccumulatorSecondCount = 0
+let valueStreamingProcessQueueSecondCount = 0
+
+const incrementStreamingIntervalCount = async () => {
+  if (Platform.OS === 'ios') {
+    const playbackSpeed = await getPlaybackSpeed()
+    valueStreamingAccumulatorSecondCount = valueStreamingAccumulatorSecondCount + 1 / playbackSpeed
+    valueStreamingProcessQueueSecondCount = valueStreamingProcessQueueSecondCount + 1 / playbackSpeed
+  } else {
+    valueStreamingAccumulatorSecondCount++
+    valueStreamingProcessQueueSecondCount++
+  }
+}
+
+const handleValueStreamingMinutePassed = async () => {
+  const globalState = getGlobal()
+  const { nowPlayingItem } = globalState.player
+
+  const valueTags = nowPlayingItem.episodeValue || nowPlayingItem.podcastValue || []
+
+  const { activeProviderSettings } = v4vGetActiveProviderInfo(valueTags)
+  const { activeProvider } = v4vGetActiveProviderInfo(getBoostagramItemValueTags(nowPlayingItem))
+  const { streamingAmount } = activeProviderSettings || {}
+
+  valueStreamingAccumulatorSecondCount = 0
+
+  // Send batch of streaming value from queue every 5 minutes
+  const shouldProcessQueue = valueStreamingProcessQueueSecondCount >= 300 
+  if (shouldProcessQueue) {
+    valueStreamingProcessQueueSecondCount = 0
+  }
+
+  if (Array.isArray(valueTags) && valueTags.length > 0 && streamingAmount && activeProvider?.key) {
+    await saveStreamingValueTransactionsToTransactionQueue(
+      valueTags,
+      nowPlayingItem,
+      streamingAmount,
+      activeProvider.key
+    )
+  }
+
+  return shouldProcessQueue
+}
 
 export const handleValueStreamingTimerIncrement = () => {
   const globalState = getGlobal()
@@ -15,18 +59,16 @@ export const handleValueStreamingTimerIncrement = () => {
 
   if (streamingValueOn) {
     playerGetState().then(async (playbackState) => {
+      let shouldProcessQueue = false
       if (playerCheckIfStateIsPlaying(playbackState)) {
-        valueStreamingIntervalSecondCount++
+        await incrementStreamingIntervalCount()
 
-        if (valueStreamingIntervalSecondCount && valueStreamingIntervalSecondCount % 60 === 0) {
-          await handleValueStreamingMinutePassed()
+        if (valueStreamingAccumulatorSecondCount && valueStreamingAccumulatorSecondCount >= 60) {
+          shouldProcessQueue = await handleValueStreamingMinutePassed()
         }
       }
 
-      // Send batch of streaming value from queue every 5 minutes
-      if (valueStreamingIntervalSecondCount === 300) {
-        valueStreamingIntervalSecondCount = 1
-
+      if (shouldProcessQueue) {
         const { errors, transactions, totalAmount } = await processValueTransactionQueue()
         if (transactions.length > 0 && totalAmount > 0) {
           setGlobal({
@@ -41,25 +83,5 @@ export const handleValueStreamingTimerIncrement = () => {
         }
       }
     })
-  }
-}
-
-const handleValueStreamingMinutePassed = async () => {
-  const globalState = getGlobal()
-  const { nowPlayingItem } = globalState.player
-
-  const valueTags = nowPlayingItem.episodeValue || nowPlayingItem.podcastValue || []
-
-  const { activeProviderSettings } = v4vGetActiveProviderInfo(valueTags)
-  const { activeProvider } = v4vGetActiveProviderInfo(getBoostagramItemValueTags(nowPlayingItem))
-  const { streamingAmount } = activeProviderSettings || {}
-
-  if (Array.isArray(valueTags) && valueTags.length > 0 && streamingAmount && activeProvider?.key) {
-    await saveStreamingValueTransactionsToTransactionQueue(
-      valueTags,
-      nowPlayingItem,
-      streamingAmount,
-      activeProvider.key
-    )
   }
 }
