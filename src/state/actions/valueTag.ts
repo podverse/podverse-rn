3071cc0c78197e7307d67f91d074cb14b77c9a38
Mitch Downey <mitchldowney@gmail.com>
2022-12-22 14:43:52 -0600

    Remove unused / deprecated initializeValueProcessor

--- src/state/actions/valueTag.ts
+++ src/state/actions/valueTag.ts
@@ -1,47 +1,10 @@
 import { getGlobal, setGlobal } from 'reactn'
-import { translate } from '../../lib/i18n'
-import { processValueTransactionQueue, setStreamingValueOn } from '../../services/v4v/v4v'
-
-let valueTransactionProcessorInterval = null
+import { setStreamingValueOn } from '../../services/v4v/v4v'
 
 /*
   initializeValueProcessor must be called after getAuthUserInfo
   so the valueTagSettings data is available on global state.
 */
-export const initializeValueProcessor = () => {
-  const globalState = getGlobal()
-  const { session } = globalState
-  const { v4v } = session
-  const hasV4VProvider = v4v.providers.connected && v4v.providers.connected.length > 0
-
-  if (hasV4VProvider) {
-    createValueTransactionProcessorInterval()
-  }
-}
-
-const clearValueTransactionProcessorInterval = () => {
-  valueTransactionProcessorInterval = null
-}
-
-const createValueTransactionProcessorInterval = () => {
-  clearValueTransactionProcessorInterval()
-
-  // eslint-disable-next-line
-  valueTransactionProcessorInterval = setInterval(async () => {
-    const { errors, transactions, totalAmount } = await processValueTransactionQueue()
-    if (transactions.length > 0 && totalAmount > 0) {
-      setGlobal({
-        bannerInfo: {
-          show: true,
-          description: translate('Streaming Value Sent'),
-          errors,
-          transactions,
-          totalAmount
-        }
-      })
-    }
-  }, 1000 * 60 * 10)
-}
 
 export const toggleValueStreaming = async () => {
   const globalState = getGlobal()
