53150c99edf1bc6f1763411b4a6386815fac93c8
Mitch Downey <mitchldowney@gmail.com>
2022-12-27 23:10:34 -0600

    Remove duplicate playerLoadNowPlayingItem from audioInitializePlayerQueue

--- src/state/actions/playerAudio.ts
+++ src/state/actions/playerAudio.ts
@@ -1,22 +1,14 @@
-import { checkIfVideoFileOrVideoLiveType, NowPlayingItem } from 'podverse-shared'
+import { NowPlayingItem } from 'podverse-shared'
 import { getGlobal, setGlobal } from 'reactn'
 import {
   audioInitializePlayerQueue as audioInitializePlayerQueueService,
   audioPlayNextFromQueue as audioPlayNextFromQueueService
 } from '../../services/playerAudio'
 import { trackPlayerScreenPageView } from '../../services/tracking'
-import { playerLoadNowPlayingItem } from './player'
 import { getQueueItems } from './queue'
 
 export const audioInitializePlayerQueue = async (item: NowPlayingItem) => {
   await audioInitializePlayerQueueService(item)
-
-  if (item && !checkIfVideoFileOrVideoLiveType(item?.episodeMediaType)) {
-    const shouldPlay = false
-    const forceUpdateOrderDate = false
-    const setCurrentItemNextInQueue = true
-    await playerLoadNowPlayingItem(item, shouldPlay, forceUpdateOrderDate, setCurrentItemNextInQueue)
-  }
 
   const globalState = getGlobal()
   setGlobal({
