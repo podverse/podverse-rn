22909d6455ac6a04751b38f4bdb54afc6b13a953
Mitch Downey <mitchldowney@gmail.com>
2023-01-26 00:57:15 -0500

    Disable PLAYER_HISTORY_INDEX_DID_UPDATE event listener

--- src/screens/HistoryIndexListenerScreen.tsx
+++ src/screens/HistoryIndexListenerScreen.tsx
@@ -7,14 +7,21 @@ import PVEventEmitter from '../services/eventEmitter'
 interface Props {}
 interface State {}
 
+/* 
+  NOTE: the PLAYER_HISTORY_INDEX_DID_UPDATE is heavy on the JS fps usage.
+  I'm commenting it out because it hampers performance and maybe we won't miss it.
+*/
 export class HistoryIndexListenerScreen extends React.Component<Props, State> {
   componentDidMount() {
     // Updates to historyItemsIndex do not force this component to re-render,
     // so we force it to re-render on the PLAYER_HISTORY_INDEX_DID_UPDATE event.
-    PVEventEmitter.on(PV.Events.PLAYER_HISTORY_INDEX_DID_UPDATE, () => this.forceUpdate())
+
+
+    // PVEventEmitter.on(PV.Events.PLAYER_HISTORY_INDEX_DID_UPDATE, () => this.forceUpdate())
   }
 
   componentWillUnmount() {
-    PVEventEmitter.removeListener(PV.Events.PLAYER_HISTORY_INDEX_DID_UPDATE)
+
+    // PVEventEmitter.removeListener(PV.Events.PLAYER_HISTORY_INDEX_DID_UPDATE)
   }
 }
