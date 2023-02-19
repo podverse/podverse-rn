a3b27620c8bc452b5d68e616dee429fa13f4daf8
Creon Creonopoulos <kreonjr@hotmail.com>
2023-02-19 00:12:33 -0600

    Added Native Android module to check Async Storage Used capacity

--- src/lib/asyncStorage.ts
+++ src/lib/asyncStorage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage'
+import { NativeModules, Platform, Alert } from 'react-native'
 
 const testKey = 'asyncStorageTestKey'
 
@@ -10,3 +11,28 @@ export const checkIfAsyncStorageIsEnabled = async () => {
   await AsyncStorage.removeItem(testKey)
   return result
 }
+
+export const checkInvalidAsyncStorageCapacity = async () => {
+  if(Platform.OS === "ios") {
+    console.log("Not implemented for ios")
+    return false
+  } else {
+      const {PVAsyncStorage} = NativeModules;
+      const returnObj = await PVAsyncStorage?.getUsedStorageSize?.()
+      if(!returnObj || returnObj.size === null || returnObj.size === undefined) {
+        throw new Error('Used size could not be read')
+      }
+
+      // 29MB is the limit for AsyncStorage on Android
+      if(returnObj.size < (29 * 1024 * 1024)) {
+        return false
+      }
+
+      Alert.alert('Warning', `You have exceed your alloted subscription size. 
+      Please unsubscribe from a few podcasts to continue`, [{text:"OK"}])
+
+      return true
+  }
+}
+
+// if(checkInvalidAsyncStorageCapacity()) return
