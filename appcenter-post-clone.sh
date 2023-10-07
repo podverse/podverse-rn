ab7ca38de283de6be7ac8387f78b5361857cd733
Mitch Downey <mitchldowney@gmail.com>
2023-10-07 00:17:15 -0500

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+printf "appcenter-post-clone script running:"
+
 # NOTE: this is a 2nd yarn install that is needed to overcome a bug
 # related to loading a remote git+ branch as a dependency in package.json
 yarn install
