e958aaf8aafdbe934b173dff6fbe3a8cc2fd0d55
Mitch Downey <mitchldowney@gmail.com>
2023-11-04 03:04:02 -0500

    blip-0010 - add time, remove pubkey
    #1991

--- src/lib/satoshiStream.ts
+++ src/lib/satoshiStream.ts
@@ -4,7 +4,7 @@
 // 7629169: SatoshiStreamStatsPodcast // the "podcast" subject according to SatoshiStream spec
 // 7629175: SatoshiStreamStatsPodcastIndexId // the Podcast Index feedId for the podcast
 
-import { SatoshiStreamStats } from 'podverse-shared'
+import { SatoshiStreamStats, convertSecToFullHHMMSS } from 'podverse-shared'
 import Config from 'react-native-config'
 import { getBuildNumber, getVersion } from 'react-native-device-info'
 import { v4vGetSenderInfo } from '../services/v4v/v4v'
@@ -36,7 +36,7 @@ export const createSatoshiStreamStats = async (
   const podcast = (podcastTitle || translate('Untitled Podcast')).substring(0, 60)
   const episode = (episodeTitle || translate('Untitled Episode')).substring(0, 60)
   const podcastIndexId = (podcastIndexPodcastId && parseInt(podcastIndexPodcastId, 10)) || null
-  const ts = parseInt(currentPlaybackPosition, 10)
+  const ts = parseInt(currentPlaybackPosition, 10) || 0
 
   // Use await and grab from AsyncStorage because there seems to be a race-condition where
   // the senderName gets passed as "Anonymous" if we grab senderName from globalState,
@@ -51,9 +51,9 @@ export const createSatoshiStreamStats = async (
       episode,
       episode_guid,
       ts,
+      time: convertSecToFullHHMMSS(ts),
       action,
       speed,
-      pubkey,
       value_msat_total: totalBatchedAmount * 1000,
       uuid: uuidv4(),
       app_name: Config.USER_AGENT_PREFIX,
