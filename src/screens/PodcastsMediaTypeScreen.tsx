27101c24274194b24b3b638ed52a96782eb6fc3e
Mitch Downey <mitchldowney@gmail.com>
2023-11-13 22:32:06 -0600

    Create PodcastsMediaTypeScreen.tsx

---
+++ src/screens/PodcastsMediaTypeScreen.tsx
@@ -0,0 +1,46 @@
+/*
+
+import React from 'react'
+
+export function withMediaType(Component: any, mediaType: string) {
+    return class extends React.Component {
+      
+      render() {
+        return <Component type={mediaType} />
+      }
+    }
+}
+
+
+
+
+
+class Albums extends React.Component {
+  render() {
+    return (
+      <>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+        <Text style={{ color: 'red' }}>{this.props.type}</Text>
+      </>
+    )
+  }  
+}
+
+export const AlbumsScreen = withMediaType(Albums, 'albums')
+
+class Videos extends React.Component {
+  render() {
+    <Text>{this.props.type}</Text>
+  }
+}
+
+export const VideosScreen = withMediaType(Albums, 'videos')
+
+*/
