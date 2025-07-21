9b1fb78635e56af95b97debdda8def831c39c544
Paul Bailey <paul@neutron.studio>
2025-07-21 16:46:53 -0500

    Fix loading on iOS CarPlay

--- src/services/playerAudioSetup.ts
+++ src/services/playerAudioSetup.ts
@@ -20,18 +20,18 @@ const iosCategoryOptions = [
 ]
 
 const setupPlayer = async (options: Parameters<typeof TrackPlayer.setupPlayer>[0]) => {
-  const setup = async () => {
+  while (1) {
     try {
       await TrackPlayer.setupPlayer(options)
     } catch (error) {
-      return (error as Error & { code?: string }).code
+      if (error?.code === 'android_cannot_setup_player_in_background') {
+        continue;
+      }
+
+      throw error;
     }
-  }
-  while ((await setup()) === 'android_cannot_setup_player_in_background') {
-    // A timeout will mostly only execute when the app is in the foreground,
-    // and even if we were in the background still, it will reject the promise
-    // and we'll try again:
-    await new Promise<void>((resolve) => setTimeout(resolve, 1))
+
+    break;
   }
 }
 
